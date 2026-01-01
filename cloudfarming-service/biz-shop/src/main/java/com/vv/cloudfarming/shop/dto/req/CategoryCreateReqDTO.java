c02086a4b6204db10ed43ba2f0beaed52f6d0ee4
choviZ <702381764@qq.com>
2026-01-01 19:38:40 +0800

    feature：分类管理操作

---
+++ cloudfarming-service/biz-shop/src/main/java/com/vv/cloudfarming/shop/dto/req/CategoryCreateReqDTO.java
@@ -0,0 +1,27 @@
+package com.vv.cloudfarming.shop.dto.req;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 创建商品分类请求参数
+ */
+@Data
+public class CategoryCreateReqDTO implements Serializable {
+
+    /**
+     * 分类名称
+     */
+    private String name;
+
+    /**
+     * 父级分类ID，顶级分类为NULL
+     */
+    private Long parentId;
+
+    /**
+     * 排序权重
+     */
+    private Integer sortOrder;
+}
---
+++ cloudfarming-service/biz-shop/src/main/java/com/vv/cloudfarming/shop/dto/req/CategoryUpdateReqDTO.java
@@ -0,0 +1,32 @@
+package com.vv.cloudfarming.shop.dto.req;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 更新商品分类请求参数
+ */
+@Data
+public class CategoryUpdateReqDTO implements Serializable {
+
+    /**
+     * 分类ID
+     */
+    private Long id;
+
+    /**
+     * 分类名称
+     */
+    private String name;
+
+    /**
+     * 父级分类ID，顶级分类为NULL
+     */
+    private Long parentId;
+
+    /**
+     * 排序权重
+     */
+    private Integer sortOrder;
+}
