9df284751b33779dabcefa3a4cfd40b68f72be14
choviZ <702381764@qq.com>
2026-02-07 17:49:27 +0800

    fix：修复接口路径不一致问题

--- services/order-pay-service/src/main/java/com/vv/cloudfarming/order/remote/ShopRemoteService.java
+++ services/order-pay-service/src/main/java/com/vv/cloudfarming/order/remote/ShopRemoteService.java
@@ -7,9 +7,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "cloudfarming-product", contextId = "shopRemoteService", url = "${aggregation.remote-url:}")
+@FeignClient(
+        value = "cloudfarming-product",
+        contextId = "shopRemoteService",
+        url = "${aggregation.remote-url:}")
 public interface ShopRemoteService {
 
-    @GetMapping("/v1/shop")
+    @GetMapping("/api/shop/get")
     Result<ShopRespDTO> getShopById(@RequestParam("shopId") @NotNull Long shopId);
 }
