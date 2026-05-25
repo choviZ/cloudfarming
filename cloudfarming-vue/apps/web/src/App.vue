aeb09e762f6eb131140027cf0270dbfe3153a327
choviZ <702381764@qq.com>
2026-05-25 10:38:34 +0800

    style：修改组件库默认配色

--- cloudfarming-vue/apps/web/src/App.vue
+++ cloudfarming-vue/apps/web/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-config-provider :locale="zhCN">
+  <a-config-provider :locale="zhCN" :theme="theme">
     <router-view />
   </a-config-provider>
 </template>
@@ -7,6 +7,12 @@
 import zhCN from 'ant-design-vue/es/locale/zh_CN'
 import { useUserStore } from '@/stores/useUserStore.js'
 import { onMounted } from 'vue'
+
+const theme = {
+  token: {
+    colorPrimary: '#16a34a',
+  },
+}
 
 const userStore = useUserStore()
 onMounted(async () => {
